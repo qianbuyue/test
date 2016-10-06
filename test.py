06de480e7b3edcfca7e165d0b0bc79a5cfbbb346
Buyue Qian <qianbuyue@xjtu.edu.cn>
2016-10-07 05:43:58 +0800

    make some errors

---
+++ test.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+def printhw():
+    '''
+    This a test function
+    '''
+    import datetime
+    import json
+    a="Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World"
+    print a
+

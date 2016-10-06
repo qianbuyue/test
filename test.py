346cf72df433e1c7b0127140e293ecb76e21b4dc
Buyue Qian <qianbuyue@xjtu.edu.cn>
2016-10-07 05:56:39 +0800

    add some errors

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ def printhw():
     '''
     This a test function
     '''
-    import datetime
-    import json
-    a = "Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World"
-    print a
+
+
+import numpy
+a = 5 + 8 + 10 + 60 + 80 * 65 + 998 + 77676 + 654654654 + 76576574 + 4687648 + 6756745 + 4765474 + 46747 + 46746765 + 64756746746
+print a
